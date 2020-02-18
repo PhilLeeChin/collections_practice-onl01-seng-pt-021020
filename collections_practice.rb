e66ba60dc486379ba601fec5c013d1533586a51d
Philip Lee-Chin <phil.leechin@outlook.com>
2020-02-18 20:13:41 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,50 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort {|a,b| b <=> a}
+end
+
+def sort_array_char_count(array)
+  array.sort do |a,b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array.sort do |a,b|
+    a[1] <=> b[2]
+  end
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(string)
+  newstr = string.split
+  newarr = []
+  newstr.each do |i|
+    newarr << "#{i.length[3]}" + "$"
+  end
+  newarr.join
+end
+
+def find_a(array)
+  array.select {|i| i.start_with?('a')}
+end
+
+def sum_array(array)
+  array.inject {|sum,i| sum += i}
+end
+
+def add_s(array)
+  array.each_with_index.collect do |element, i|
+    if i != 1
+      element + "s"
+    else
+      element
+    end
+  end
+end
